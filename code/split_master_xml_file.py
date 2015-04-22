d6f2ca21099ea78c9f89f5720eafbb26b661750e
Maximilian Albert <maximilian.albert@gmail.com>
2015-04-22 12:33:57 +0100

    Add helper function to check the type of an XML element.

--- code/split_master_xml_file.py
+++ code/split_master_xml_file.py
@@ -1,16 +1,21 @@
 import xml.etree.ElementTree as ET
 
 
+def check_xml_type(element, xml_type):
+    if not isinstance(element, ET.Element):
+        raise TypeError("Not an XML element: '{}' (type: {})".format(element, type(element)))
+
+    if element.tag != xml_type:
+        raise TypeError("XML Element is not of type '{}': {}".format(xml_type, element))
+
+
 def is_initial_measure(element):
     """
     Return `True` if the XML element is of type 'measure' and has the
     attribute "number='1'". Otherwise return `False`.
 
     """
-    assert isinstance(element, ET.Element)
-
-    if element.tag != 'measure':
-        raise TypeError("XML Element is not of type 'measure': {}".format(element))
+    check_xml_type(element, 'measure')
 
     attribs = element.attrib
     try:
--- code/split_master_xml_file_test.py
+++ code/split_master_xml_file_test.py
@@ -4,7 +4,22 @@ import xml.etree.ElementTree as ET
 from split_master_xml_file import *
 
 
+def test_check_xml_type():
+    # These should pass
+    check_xml_type(ET.fromstring("<measure><note></note></measure>"), 'measure')
+    check_xml_type(ET.fromstring("<note></note>"), 'note')
+
+    for x in ['foo', 42, ET.fromstring("<note></note>")]:
+        with pytest.raises(TypeError):
+            check_xml_type(x, 'measure')
+
+
 def test_is_initial_measure():
+    """
+    Check whether a measure is the initial measure of a chant. This is
+    based purely on whether the measure number is '1'.
+
+    """
     m1 = ET.fromstring("<measure number='1'></measure>")
     m2 = ET.fromstring("<measure number='3'></measure>")
     m3 = ET.fromstring("<measure number='42'></measure>")
